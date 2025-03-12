f9593fdc2a134f7105fdddd47e0f1bde5d3d8db6
Rainold2004 <benitezrainold68@gmail.com>
2025-03-11 21:32:16 -0400

    primera versión1

---
+++ script.js
@@ -0,0 +1,7 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const icon = darkModeToggle.querySelector('i');
+    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('fa-sun');
+});
